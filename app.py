4179782c9f03e90afe75c8fbbd7f83892c8815b7
Samir Amin <aminsamir45@gmail.com>
2023-07-10 11:20:08 -0700

    a

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from operator import index
 import streamlit as st
 import plotly.express as px
-# from pycaret.regression import * #setup, compare_models, pull, save_model, load_model
+from pycaret.regression import * #setup, compare_models, pull, save_model, load_model
 import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
@@ -11,8 +11,8 @@ if os.path.exists('./dataset.csv'):
     df = pd.read_csv('dataset.csv', index_col=None)
 
 with st.sidebar: 
-    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
-    st.title("AutoNickML")
+    st.image("https://spectrum.ieee.org/media-library/brain-inspired-computing.jpg?id=25581603&width=980")
+    st.title("Auto Machine Learning")
     choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
     st.info("This project application helps you build and explore your data.")
 
